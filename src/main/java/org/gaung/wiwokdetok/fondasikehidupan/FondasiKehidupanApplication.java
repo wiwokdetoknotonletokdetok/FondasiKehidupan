9452ebe80861ff2a8357a85dcf8416fe54b44241
FransiscaEllyaB <ellyafransiscab@gmail.com>
2025-06-27 16:41:24 +0700

    Initialize springboot

---
+++ src/main/java/org/gaung/wiwokdetok/fondasikehidupan/FondasiKehidupanApplication.java
@@ -0,0 +1,13 @@
+package org.gaung.wiwokdetok.fondasikehidupan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FondasiKehidupanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FondasiKehidupanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gaung/wiwokdetok/fondasikehidupan/FondasiKehidupanApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gaung.wiwokdetok.fondasikehidupan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FondasiKehidupanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
