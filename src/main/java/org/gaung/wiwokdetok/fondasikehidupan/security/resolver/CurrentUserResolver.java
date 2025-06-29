5599ed36fc9cbe9539923821f9b895d8243ca3a2
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-06-30 02:03:22 +0700

    [GREEN] User Resolver

---
+++ src/main/java/org/gaung/wiwokdetok/fondasikehidupan/security/resolver/CurrentUserResolver.java
@@ -0,0 +1,65 @@
+package org.gaung.wiwokdetok.fondasikehidupan.security.resolver;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.gaung.wiwokdetok.fondasikehidupan.dto.UserPrincipal;
+import org.gaung.wiwokdetok.fondasikehidupan.security.JwtUtil;
+import org.gaung.wiwokdetok.fondasikehidupan.security.annotation.CurrentUser;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.server.ResponseStatusException;
+
+@Component
+public class CurrentUserResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtUtil jwtUtil;
+
+    public CurrentUserResolver(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(CurrentUser.class) != null &&
+                parameter.getParameterType().equals(UserPrincipal.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  org.springframework.web.bind.support.WebDataBinderFactory binderFactory) {
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        String token = request.getHeader("Authorization");
+
+        if (token == null || !token.startsWith("Bearer ")) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Missing or invalid token");
+        }
+
+        token = token.substring(7);
+
+        Claims payload;
+
+        try {
+            payload = jwtUtil.decodeToken(token);
+        } catch (ExpiredJwtException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Token expired");
+        } catch (JwtException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid token");
+        }
+
+        String userId = jwtUtil.getId(payload);
+        String role = jwtUtil.getRole(payload);
+
+        return new UserPrincipal(userId, role);
+    }
+}
