c4d719b718e8c092cab1d7fb7e32c87f1c6119f6
FransiscaEllyaB <ellyafransiscab@gmail.com>
2025-07-01 22:34:51 +0700

    [REFACTOR] Change type error

--- src/main/java/org/gaung/wiwokdetok/fondasikehidupan/service/ReviewService.java
+++ src/main/java/org/gaung/wiwokdetok/fondasikehidupan/service/ReviewService.java
@@ -4,9 +4,11 @@ import org.gaung.wiwokdetok.fondasikehidupan.dto.ReviewRequestDTO;
 import org.gaung.wiwokdetok.fondasikehidupan.dto.ReviewResponseDTO;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface ReviewService {
     ReviewResponseDTO submitReview(ReviewRequestDTO dto);
     ReviewResponseDTO updateReview(ReviewRequestDTO dto);
     List<ReviewResponseDTO> getReviewsForBook(Long bookId);
+    void deleteReview(UUID userId, Long bookId);
 }
