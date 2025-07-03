42732f28f067b739397aa1fb6861d72a7baa7be7
FransiscaEllyaB <ellyafransiscab@gmail.com>
2025-07-03 12:19:13 +0700

    [REFACTOR] Change Type Error

--- src/main/java/org/gaung/wiwokdetok/fondasikehidupan/controller/BookController.java
+++ src/main/java/org/gaung/wiwokdetok/fondasikehidupan/controller/BookController.java
@@ -1,13 +1,21 @@
 package org.gaung.wiwokdetok.fondasikehidupan.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.gaung.wiwokdetok.fondasikehidupan.dto.*;
+import org.gaung.wiwokdetok.fondasikehidupan.dto.BookRequestDTO;
+import org.gaung.wiwokdetok.fondasikehidupan.dto.BookResponseDTO;
+import org.gaung.wiwokdetok.fondasikehidupan.dto.BookSummaryDTO;
+import org.gaung.wiwokdetok.fondasikehidupan.dto.WebResponse;
 import org.gaung.wiwokdetok.fondasikehidupan.security.annotation.AllowedRoles;
-import org.gaung.wiwokdetok.fondasikehidupan.security.annotation.CurrentUser;
 import org.gaung.wiwokdetok.fondasikehidupan.service.BookService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -20,43 +28,31 @@ public class BookController {
 
     @PostMapping
     @AllowedRoles({"USER"})
-    public ResponseEntity<WebResponse<BookResponseDTO>> createBook(@RequestBody BookRequestDTO dto) {
-
-        BookResponseDTO created = bookService.createBook(dto);
+    public ResponseEntity<WebResponse<String>> createBook(@RequestBody BookRequestDTO dto) {
+        bookService.createBook(dto);
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(WebResponse.<BookResponseDTO>builder()
-                        .data(created)
+                .body(WebResponse.<String>builder()
+                        .data("OK")
                         .build());
     }
 
     @GetMapping
-    @AllowedRoles({"USER"})
     public ResponseEntity<WebResponse<List<BookSummaryDTO>>> getAllBooks() {
         return ResponseEntity.ok(WebResponse.<List<BookSummaryDTO>>builder()
                 .data(bookService.getAllBooks())
                 .build());
     }
 
-    @GetMapping("/search")
-    @AllowedRoles({"USER"})
-    public ResponseEntity<WebResponse<List<BookSummaryDTO>>> searchByTitle(@RequestParam("q") String keyword) {
-        return ResponseEntity.ok(WebResponse.<List<BookSummaryDTO>>builder()
-                .data(bookService.findByTitleContainingIgnoreCase(keyword))
-                .build());
-    }
-
     @GetMapping("/{id}")
-    @AllowedRoles({"USER"})
     public ResponseEntity<WebResponse<BookResponseDTO>> getBookById(@PathVariable Long id) {
         return ResponseEntity.ok(WebResponse.<BookResponseDTO>builder()
                 .data(bookService.getBookById(id))
                 .build());
     }
 
-    @GetMapping("/advanced-search")
-    @AllowedRoles({"USER"})
+    @GetMapping("/search")
     public ResponseEntity<WebResponse<List<BookSummaryDTO>>> advancedSearch(
             @RequestParam(required = false) String title,
             @RequestParam(required = false) String isbn,
@@ -69,6 +65,9 @@ public class BookController {
         if ("".equals(author)) author = null;
         if ("".equals(genre)) genre = null;
         if ("".equals(publisher)) publisher = null;
+        System.out.print("After null check: ");
+        System.out.printf("title: %s, isbn: %s, author: %s, genre: %s, publisher: %s%n",
+                title, isbn, author, genre, publisher);
 
         return ResponseEntity.ok(WebResponse.<List<BookSummaryDTO>>builder()
                 .data(bookService.advancedSearch(title, isbn, author, genre, publisher))
