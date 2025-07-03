2c595ee436b4d1ea731ab78f5718a78724b49033
FransiscaEllyaB <ellyafransiscab@gmail.com>
2025-07-03 12:39:32 +0700

    [GREEN] Add new validation

--- src/main/java/org/gaung/wiwokdetok/fondasikehidupan/dto/ReviewRequestDTO.java
+++ src/main/java/org/gaung/wiwokdetok/fondasikehidupan/dto/ReviewRequestDTO.java
@@ -1,5 +1,8 @@
 package org.gaung.wiwokdetok.fondasikehidupan.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +15,11 @@ import java.util.UUID;
 public class ReviewRequestDTO {
     private UUID userId;
     private Long bookId;
+
+    @NotBlank(message = "Pesan review tidak boleh kosong")
     private String message;
+
+    @Min(value = 0, message = "Rating minimal adalah 0")
+    @Max(value = 5, message = "Rating maksimal adalah 5")
     private int rating;
 }
