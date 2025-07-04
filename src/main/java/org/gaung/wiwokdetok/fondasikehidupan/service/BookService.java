6333718d348c20bf67afc9f151a1039b28286d2b
FransiscaEllyaB <ellyafransiscab@gmail.com>
2025-07-04 10:24:46 +0700

    [REFACTOR] change type

--- src/main/java/org/gaung/wiwokdetok/fondasikehidupan/service/BookService.java
+++ src/main/java/org/gaung/wiwokdetok/fondasikehidupan/service/BookService.java
@@ -7,7 +7,7 @@ import org.gaung.wiwokdetok.fondasikehidupan.dto.BookSummaryDTO;
 import java.util.List;
 
 public interface BookService {
-    BookResponseDTO createBook(BookRequestDTO dto);
+    void createBook(BookRequestDTO dto);
     List<BookSummaryDTO> getAllBooks();
     BookResponseDTO getBookById(Long idBook);
     List<BookSummaryDTO> advancedSearch(String title, String isbn, String author, String genre, String publisher);
