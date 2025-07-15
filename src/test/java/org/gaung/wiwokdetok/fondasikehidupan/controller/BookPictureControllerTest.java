3a7819b01f9b30b82e0673b3c985863cbd0830c0
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-07-15 21:34:44 +0700

    [RED] Update test

--- src/test/java/org/gaung/wiwokdetok/fondasikehidupan/controller/BookPictureControllerTest.java
+++ src/test/java/org/gaung/wiwokdetok/fondasikehidupan/controller/BookPictureControllerTest.java
@@ -8,13 +8,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import org.gaung.wiwokdetok.fondasikehidupan.dto.WebResponse;
-import org.gaung.wiwokdetok.fondasikehidupan.model.Book;
-import org.gaung.wiwokdetok.fondasikehidupan.model.Publisher;
-import org.gaung.wiwokdetok.fondasikehidupan.repository.BookRepository;
-import org.gaung.wiwokdetok.fondasikehidupan.repository.PublisherRepository;
 import org.gaung.wiwokdetok.fondasikehidupan.security.JwtUtil;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -28,8 +22,6 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.time.OffsetDateTime;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -55,37 +47,7 @@ public class BookPictureControllerTest {
     private AmazonS3 amazonS3;
 
     @Autowired
-    private BookRepository bookRepository;
-
-    @Autowired
-    private PublisherRepository publisherRepository;
-
-    @Autowired
     private ObjectMapper objectMapper;
-
-    private Book book;
-
-    @BeforeEach
-    void setUp() {
-        Publisher publisher = new Publisher();
-        publisher.setName("publisher");
-        publisherRepository.save(publisher);
-
-        book = new Book();
-        book.setTitle("Book Title");
-        book.setIsbn("978-0-306-40615-7");
-        book.setSynopsis("Book Synopsis");
-        book.setBookPicture("https://example.com");
-        book.setRating(BigDecimal.valueOf(0.0));
-        book.setCreatedAt(OffsetDateTime.now());
-        book.setPublisher(publisher);
-    }
-
-    @AfterEach
-    void tearDown() {
-        bookRepository.deleteAll();
-        publisherRepository.deleteAll();
-    }
 
     private MockMultipartFile generateDummyPicture() throws IOException {
         BufferedImage dummyImage = new BufferedImage(500, 500, BufferedImage.TYPE_INT_RGB);
@@ -145,83 +107,6 @@ public class BookPictureControllerTest {
         ).andDo(result -> {
             WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
             });
-            assertNull(response.getData());
-            assertNotNull(response.getErrors());
-        });
-    }
-
-    @Test
-    void testUpdateBookPictureSuccess() throws Exception {
-        Claims payload = mock(Claims.class);
-        when(jwtUtil.decodeToken("valid.token.here")).thenReturn(payload);
-        when(jwtUtil.getId(payload)).thenReturn(UUID.randomUUID());
-        when(jwtUtil.getRole(payload)).thenReturn("USER");
-
-        MockMultipartFile multipartFile = generateDummyPicture();
-
-        when(amazonS3.putObject(any(PutObjectRequest.class)))
-                .thenReturn(new PutObjectResult());
-
-        mockMvc.perform(
-                multipart("/books/{bookId}}/book-picture", book.getId())
-                        .file(multipartFile)
-                        .header("Authorization", "Bearer valid.token.here")
-                        .contentType(MediaType.MULTIPART_FORM_DATA)
-                        .accept(MediaType.APPLICATION_JSON)
-        ).andExpectAll(
-                status().isCreated(),
-                header().exists("Location")
-        ).andDo(result -> {
-            WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {}
-            );
-            assertNotNull(response.getData());
-            assertNull(response.getErrors());
-        });
-    }
-
-    @Test
-    void testUpdateBookPictureFailedWhenBookIsNotExist() throws Exception {
-        Claims payload = mock(Claims.class);
-        when(jwtUtil.decodeToken("valid.token.here")).thenReturn(payload);
-        when(jwtUtil.getId(payload)).thenReturn(UUID.randomUUID());
-        when(jwtUtil.getRole(payload)).thenReturn("USER");
-
-        MockMultipartFile multipartFile = generateDummyPicture();
-
-        when(amazonS3.putObject(any(PutObjectRequest.class)))
-                .thenReturn(new PutObjectResult());
-
-        mockMvc.perform(
-                multipart("/books/{bookId}}/book-picture", 12841489)
-                        .file(multipartFile)
-                        .header("Authorization", "Bearer valid.token.here")
-                        .contentType(MediaType.MULTIPART_FORM_DATA)
-                        .accept(MediaType.APPLICATION_JSON)
-        ).andExpectAll(
-                status().isNotFound()
-        ).andDo(result -> {
-            WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {}
-            );
-            assertNull(response.getData());
-            assertNotNull(response.getErrors());
-        });
-    }
-
-    @Test
-    void testUpdateBookPictureFailedWhenTokenIsInvalid() throws Exception {
-        when(jwtUtil.decodeToken("invalid.token.here"))
-                .thenThrow(new JwtException("Invalid token"));
-
-        mockMvc.perform(
-                multipart("/books/{bookId}}/book-picture", book.getId())
-                        .header("Authorization", "Bearer invalid.token.here")
-                        .contentType(MediaType.MULTIPART_FORM_DATA)
-                        .accept(MediaType.APPLICATION_JSON)
-        ).andExpectAll(
-                status().isUnauthorized()
-        ).andDo(result -> {
-            WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {}
-            );
             assertNull(response.getData());
             assertNotNull(response.getErrors());
         });
