7360725b36dbb71a80311aec4e25d746e6fdd9c0
FransiscaEllyaB <ellyafransiscab@gmail.com>
2025-07-19 12:28:13 +0700

    [REFACTOR] Remove Unused Import

--- src/test/java/org/gaung/wiwokdetok/fondasikehidupan/controller/GenreControllerTest.java
+++ src/test/java/org/gaung/wiwokdetok/fondasikehidupan/controller/GenreControllerTest.java
@@ -1,7 +1,6 @@
 package org.gaung.wiwokdetok.fondasikehidupan.controller;
 
 import org.gaung.wiwokdetok.fondasikehidupan.service.GenreService;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
